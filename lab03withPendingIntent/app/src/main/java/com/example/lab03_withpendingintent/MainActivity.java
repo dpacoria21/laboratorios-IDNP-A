9ef2df7e490511496437fb6ec59d318365f344f5
Diego Pacori <dpacoria@unsa.edu.pe>
2025-10-05 22:44:54 -0500

    I add PendingIntent in the app

--- lab03withPendingIntent/app/src/main/java/com/example/lab03_withpendingintent/MainActivity.java
+++ lab03withPendingIntent/app/src/main/java/com/example/lab03_withpendingintent/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.lab03_withpendingintent;
 
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -20,8 +17,6 @@ import com.example.lab03_withpendingintent.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BatteryBroadcastReceiver  batteryBroadcastReceiver = new BatteryBroadcastReceiver();
-    private IntentFilter intentFilter = new IntentFilter();
     private ActivityMainBinding binding;
 
     TextView porcentaje, voltaje, salud, tipoBateria, tipoCarga, temp, estadoCarga;
@@ -33,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+
         porcentaje = binding.textCntPorcentaje;
         voltaje = binding.textCntVoltaje;
         salud = binding.textCntSalud;
@@ -41,54 +36,57 @@ public class MainActivity extends AppCompatActivity {
         estadoCarga = binding.textCntEstadoCarga;
         tipoBateria = binding.textCntTipo;
         tipoCarga = binding.textCntEstadoCarga;
+
         readFromFile();
     }
 
     @Override
-    protected void onPause() {
-        unregisterReceiver(batteryBroadcastReceiver);
-        Log.d(TAG, "BroadcastReceiver desregistrado satisfactoriamente.");
-        super.onPause();
-    }
-
-    @Override
     protected void onResume() {
-        registerReceiver(batteryBroadcastReceiver, intentFilter);
-        Log.d(TAG, "BroadcastReceiver registrado satisfactoriamente.");
         super.onResume();
+        // Creamos el PendingIntent que lanzará el BroadcastReceiver
+        Intent batteryIntent = new Intent(this, BatteryBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                this,
+                0,
+                batteryIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+        );
+
+        // Recuperamos el estado de la batería sin registrar un BroadcastReceiver directamente
+        Intent batteryStatus = registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        if (batteryStatus != null) {
+            try {
+                pendingIntent.send(this, 0, batteryStatus);
+            } catch (PendingIntent.CanceledException e) {
+                e.printStackTrace();
+            }
+        }
+        Log.d(TAG, "PendingIntent enviado al BroadcastReceiver.");
+        readFromFile();
     }
 
-    private void readFromFile(){
+    private void readFromFile() {
         try {
             FileInputStream fileInputStream = openFileInput("datos.txt");
             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             StringBuilder stringBuilder = new StringBuilder();
-            String text="";
+            String text = "";
             while ((text = bufferedReader.readLine()) != null) {
                 stringBuilder.append(text).append("\n");
             }
             String[] data = stringBuilder.toString().split("\n");
-            Log.v("textAAAA",data[4]);
-            porcentaje.setText(data[0]);
-            voltaje.setText(data[1]);
-            salud.setText(data[2]);
-            tipoBateria.setText(data[3]);
-            tipoCarga.setText(data[4]);
-            temp.setText(data[5]);
-            estadoCarga.setText(data[6]);
+            if (data.length >= 7) {
+                porcentaje.setText(data[0]);
+                voltaje.setText(data[1]);
+                salud.setText(data[2]);
+                tipoBateria.setText(data[3]);
+                tipoCarga.setText(data[4]);
+                temp.setText(data[5]);
+                estadoCarga.setText(data[6]);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-        /*
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        */
 }
